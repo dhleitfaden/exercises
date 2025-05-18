21b660ed488c762878b64c01d8268671c3d8a4f5
DHLeitfaden <212041979+dhleitfaden@users.noreply.github.com>
2025-05-18 18:46:56 +0200

    Add files via upload

---
+++ Reconstructing Intellectual Biographies from Fragmentary Data.py
@@ -0,0 +1,72 @@
+individuals = [
+    {
+        "name": "Gottfried Wilhelm Leibniz",
+        "activity": "publication",
+        "location": "Leipzig",
+        "year": 1666,
+        "details": "Disputatio Metaphysica de Principio Individui"
+    },
+    {
+        "name": "G. W. Leibniz",
+        "activity": "correspondence",
+        "location": "Paris",
+        "year": 1672,
+        "details": "Letter to Arnauld"
+    },
+    {
+        "name": "I. Newton",
+        "activity": "publication",
+        "location": "London",
+        "year": 1687,
+        "details": "Philosophiæ Naturalis Principia Mathematica"
+    },
+    {
+        "name": "Isaac Newton",
+        "activity": "appointment",
+        "location": "Cambridge",
+        "year": 1669,
+        "details": "Lucasian Professor of Mathematics"
+    },
+    {
+        "name": "Edmond Halley",
+        "activity": "correspondence",
+        "location": "London",
+        "year": 1686,
+        "details": "Letter to Newton encouraging publication"
+    },
+    {
+        "name": "B. Spinoza",
+        "activity": "publication",
+        "location": "The Hague",
+        "year": 1677,
+        "details": "Ethica"
+    },
+    {
+        "name": "Baruch Spinoza",
+        "activity": "correspondence",
+        "location": "Amsterdam",
+        "year": 1665,
+        "details": "Letter to Henry Oldenburg"
+    },
+    {
+        "name": "René Descartes",
+        "activity": "publication",
+        "location": "Leiden",
+        "year": 1637,
+        "details": "Discours de la méthode"
+    },
+    {
+        "name": "R. Descartes",
+        "activity": "correspondence",
+        "location": "Stockholm",
+        "year": 1649,
+        "details": "Letter to Queen Christina of Sweden"
+    },
+    {
+        "name": "Antoine Arnauld",
+        "activity": "correspondence",
+        "location": "Paris",
+        "year": 1672,
+        "details": "Letter to Leibniz"
+    }
+]
